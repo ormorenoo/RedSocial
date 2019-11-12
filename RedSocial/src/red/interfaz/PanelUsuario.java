bd96c9b64fd6243d9470cdd53006c061bbc9c84b
Anjully <tatianamora1902@gmail.com>
2019-11-12 11:25:12 -0500

    COMPARTIR PUBLICACIONES
    en la ventana panel de usuario obtuvimos el mensaje a publicar y se mando a la ventana de oublicaciones donde apareceran todos aquellos mensajes

--- RedSocial/src/red/interfaz/PanelUsuario.java
+++ RedSocial/src/red/interfaz/PanelUsuario.java
@@ -1,30 +1,24 @@
 package red.interfaz;
 
 import java.awt.BorderLayout;
-import java.awt.EventQueue;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
 
-import javax.swing.JFrame;
+import javax.swing.JButton;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 import javax.swing.JTabbedPane;
+import javax.swing.JTable;
+import javax.swing.JTextField;
 import javax.swing.border.EmptyBorder;
-import javax.swing.table.TableColumn;
 
-import red.modelo.Mensaje;
 import red.modelo.Nodo;
 import red.modelo.Publicacion;
 import red.modelo.Usuario;
 
-import javax.swing.JButton;
-import javax.swing.JLabel;
-import javax.swing.JTextPane;
-import javax.swing.JTable;
-import javax.swing.JScrollPane;
-import javax.swing.JTextField;
-
-public class PanelUsuario extends JPanel implements ActionListener{
+public class PanelUsuario extends JPanel implements ActionListener {
 
 	private JPanel contentPane;
 	private int i = 2;
@@ -36,123 +30,127 @@ public class PanelUsuario extends JPanel implements ActionListener{
 	private JButton btnEnviarSolicitud;
 	private JButton btnEnviarMensaje;
 	private Usuario user;
-
+	private JTextField textFieldPublicacion;
+	private JButton btnPublicar;
+	private String mensaje;
+	private VentanaPublicacion miVentanaPublicacion;
 
 	/**
 	 * Create the frame.
 	 */
-	public PanelUsuario(VentanaPrincipal principal, Usuario user) 
-	{
+	public PanelUsuario(VentanaPrincipal principal, Usuario user) {
 		this.principal = principal;
 		this.user = user;
 		setBounds(100, 100, 429, 302);
 		setLayout(null);
-		
+
 		btnAtras = new JButton("Atras");
 		btnAtras.addActionListener(this);
 		btnAtras.setBounds(10, 266, 76, 23);
 		add(btnAtras);
-		
+
 		btnEnviarMensaje = new JButton("Enviar Mensaje");
 		btnEnviarMensaje.addActionListener(this);
 		btnEnviarMensaje.setBounds(121, 266, 124, 23);
 		add(btnEnviarMensaje);
-		
+
 		txtNombreUsuario = new JTextField();
 		txtNombreUsuario.setText("Nombre usuario");
 		txtNombreUsuario.setBounds(10, 75, 111, 20);
 		add(txtNombreUsuario);
 		txtNombreUsuario.setColumns(10);
-		
+
 		btnEnviarSolicitud = new JButton("Enviar Solicitud");
 		btnEnviarSolicitud.addActionListener(this);
 		btnEnviarSolicitud.setBounds(10, 106, 111, 23);
 		add(btnEnviarSolicitud);
-		
+
+		textFieldPublicacion = new JTextField();
+		textFieldPublicacion.setBounds(193, 75, 206, 44);
+		add(textFieldPublicacion);
+		textFieldPublicacion.setColumns(10);
+
+		btnPublicar = new JButton("publicar");
+		btnPublicar.addActionListener(this);
+		btnPublicar.setBounds(310, 138, 89, 23);
+		add(btnPublicar);
+
 		JLabel lblNoHayMensajes = new JLabel("No hay mensajes para mostrar");
-		
+
 		JScrollPane scrollPane = new JScrollPane();
-		
-		if(user != null)
-		{
-			String[] columnas = {"Notificaciones", "Fecha", "Usuario"};
+
+		if (user != null) {
+			String[] columnas = { "Notificaciones", "Fecha", "Usuario" };
 			ArrayList<Publicacion> msj = user.getMuro().getMensajes();
 			Object[][] inf = new Object[msj.size()][3];
-			if(msj.isEmpty())
-			{
+			if (msj.isEmpty()) {
 				lblNoHayMensajes.setBounds(145, 128, 183, 14);
 				add(lblNoHayMensajes);
-			}else
-			{
-				
+			} else {
+
 				scrollPane.setBounds(111, 122, 2, 2);
 				add(scrollPane);
-				
+
 				int aux = 0;
 				int aux2 = 0;
 				int aux3 = 0;
-				for(int i = 0 ; i < inf.length ; i++)
-				{
-					for(int j = 0 ; j < inf[0].length ; j++)
-					{
-						if(j == 0)
-						{
+				for (int i = 0; i < inf.length; i++) {
+					for (int j = 0; j < inf[0].length; j++) {
+						if (j == 0) {
 							inf[i][j] = msj.get(aux).getTexto();
 							aux++;
-						}else
-						{
-							if(j == 1)
-							{
+						} else {
+							if (j == 1) {
 								inf[i][j] = msj.get(aux2).getFecha();
 								aux2++;
-							}else
-							{
+							} else {
 								inf[i][j] = msj.get(aux3).getUserOrigen();
 							}
 						}
-						
+
 					}
 				}
-			
+
 			}
-			
+
 			table = new JTable(inf, columnas);
 			table.setBounds(45, 162, 345, -115);
 			scrollPane.add(table);
-			
-		}else
-		{
+
+		} else {
 			JLabel lblNoHayUsuarios = new JLabel("No hay usuarios para mostrar");
 			lblNoHayUsuarios.setBounds(145, 128, 183, 14);
 			add(lblNoHayUsuarios);
 		}
-		
+
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		contentPane.setLayout(new BorderLayout(0, 0));
 	}
 
-
 	@Override
-	public void actionPerformed(ActionEvent e) 
-	{
-		if(e.getSource() ==btnAtras)
-		{
+	public void actionPerformed(ActionEvent e) {
+		if (e.getSource() == btnAtras) {
 			principal.ocultar();
 		}
-		
-		if(e.getSource() == btnEnviarSolicitud)
-		{
+
+		if (e.getSource() == btnEnviarSolicitud) {
 			String nombre = txtNombreUsuario.getText();
-			
-			if(principal.buscarUsuario(nombre) != null)
-			{
+
+			if (principal.buscarUsuario(nombre) != null) {
 				Nodo origen = principal.buscarNodo(user.getNick());
 				Usuario d = principal.buscarUsuario(txtNombreUsuario.getText());
 				Nodo destino = principal.buscarNodo(d.getNick());
 				VentanaSolicitud solicitud = new VentanaSolicitud(origen, destino);
 				solicitud.setVisible(true);
 			}
+		}
+
+		if (e.getSource() == btnPublicar) {
+			mensaje = textFieldPublicacion.getText();
+			VentanaPublicacion principal = new VentanaPublicacion(mensaje);
+			principal.setVisible(true);
+			this.setVisible(false);
 		}
 	}
 }
--- RedSocial/src/red/interfaz/VentanaPublicacion.java
+++ RedSocial/src/red/interfaz/VentanaPublicacion.java
@@ -1,75 +1,76 @@
 package red.interfaz;
 
-import java.awt.BorderLayout;
-import java.awt.EventQueue;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
-import javax.swing.JButton;
-import java.awt.event.ActionListener;
-import java.awt.event.ActionEvent;
-import javax.swing.JLabel;
 
-public class VentanaPublicacion extends JFrame implements ActionListener{
+public class VentanaPublicacion extends JFrame implements ActionListener {
 
 	private JPanel contentPane;
 	private JButton btnMeGusta;
 	private int cont = 0;
 	private JLabel lblM;
-
-	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					VentanaPublicacion frame = new VentanaPublicacion();
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
+	private PanelUsuario panelUsuario;
+	private JLabel lblNewLabel;
+	private String mensaje;
+	private JButton btnAtras;
+	private JLabel lblMeGustas;
 
 	/**
 	 * Create the frame.
 	 */
-	public VentanaPublicacion() {
+	public VentanaPublicacion(String mensaje) {
+
+		this.mensaje = mensaje;
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
-		
+
 		btnMeGusta = new JButton("Me gusta");
 		btnMeGusta.addActionListener(this);
-		btnMeGusta.setBounds(137, 143, 89, 23);
+		btnMeGusta.setBounds(112, 114, 89, 23);
 		contentPane.add(btnMeGusta);
-		
-		JLabel lblMeGustas = new JLabel("Me gustas :");
-		lblMeGustas.setBounds(137, 95, 76, 14);
+
+		lblMeGustas = new JLabel("Me gustas :");
+		lblMeGustas.setBounds(10, 118, 76, 14);
 		contentPane.add(lblMeGustas);
-		
-		lblM = new JLabel(cont+"");
-		lblM.setBounds(223, 95, 46, 14);
+
+		lblM = new JLabel(cont + "");
+		lblM.setBounds(71, 118, 33, 14);
 		contentPane.add(lblM);
+
+		lblNewLabel = new JLabel(mensaje);
+		lblNewLabel.setBounds(10, 57, 363, 50);
+		contentPane.add(lblNewLabel);
+
+		btnAtras = new JButton("atras");
+		btnAtras.addActionListener(this);
+		btnAtras.setBounds(318, 216, 89, 23);
+		contentPane.add(btnAtras);
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		
-		if(e.getSource() == btnMeGusta)
-		{
-			if(cont == 0)
-			{
+
+		if (e.getSource() == btnMeGusta) {
+			if (cont == 0) {
 				System.out.println("Me gusta");
 				cont++;
-				lblM.setText(cont+"");
+				lblM.setText(cont + "");
 			}
+		}
+
+		if (e.getSource() == btnAtras) {
+
+			this.setVisible(false);
 		}
 	}
 }
